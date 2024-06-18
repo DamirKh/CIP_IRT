6432e0e10fa833f080726b717a9aff1b3d9e119f
Damir Khakimov <damir.hakimov@gmail.com>
2024-06-18 17:08:13 +0400

    не получает серийный номер шасси через контролнет

--- scanner_lib.py
+++ scanner_lib.py
@@ -7,48 +7,35 @@ from pycomm3.exceptions import ResponseError, RequestError, CommError
 
 from global_data import Entry_point
 
-scanned_bp = set([])
-scanned_cn = set([])
+bp_all = set([])
+full_map = {}
+
+controlnet_module = 22
+intellectual_module = (
+    22,  # controlnet
+    166,  # ethernet
+)
+plc_module = 93
+serial_unknown = 'FFFFFFFF'
 
 
-class Discover(object):
+class AlreadyScanned(Exception):
+    """Custom exception to indicate a backplane has already been scanned."""
+
+    def __init__(self, bp_serial):
+        super().__init__(f"Backplane with serial {bp_serial} has already been scanned.")
+
+
+class BackplaneSerialNumberMissmatch(Exception):
+    """an exception occurs when the backplane serial number does not match the current serial number"""
+
+    def __init__(self, bp_serial_current, bp_serial_prev):
+        super().__init__(f"Backplane SN {bp_serial_current} != {bp_serial_prev}")
+
+
+def scan_bp(cip_path, format='', exclude_bp_sn=''):
     """
-    Scans a whole system
-    """
-
-    def __init__(self, entry_point):
-        """
-        Initializes the Discover object.
-
-        Args:
-            entry_point (str): The IP address or hostname of the entry point for scanning.
-        """
-        self.entry_point = entry_point
-        self.discovered_nodes = {}  # {serial_number: (cip_path, scanned)}
-        self.bps = {}
-        self.cn_nodes = []
-        self.scan_complete = False
-
-
-    def run(self):
-        """
-        """
-        bp_sn, fs, devices = scan_bp(self.entry_point)
-        self.bps[bp_sn] = devices
-
-        for controlnet_sn in fs.keys():
-            cip_path, already_scanned = fs[controlnet_sn]
-            if not already_scanned:
-                nodes = scan_cn(cip_path)
-            # print(nodes)
-
-
-        self.cn_nodes = nodes
-
-
-def scan_bp(cip_path):
-    """
-    Scans the specified CIP path for modules and returns a dictionary
+    Scans the Backplane by specified CIP path for modules and returns a dictionary
     mapping their serial numbers to their corresponding paths and whether they've been scanned.
 
     Args:
@@ -59,36 +46,31 @@ def scan_bp(cip_path):
               and values are tuples containing the module's path and a boolean indicating
               whether it's been scanned.
     """
-    future_scan = {}
-    backplane_sn = None
-    devices = []
+    print(f'{format}Scanning backplane at {cip_path}')
+
+    this_bp = {}
+    this_bp_sn = serial_unknown
+    modules_all = set([])
+    cn_modules_paths = {}
+
+    # devices = []
 
     driver = CIPDriver(cip_path)
     driver.open()
     for slot in range(14):
         try:
             device = driver.get_module_info(slot)
+            this_bp[slot] = ic(device)
             module_serial_number = device['serial']
+            modules_all.add(module_serial_number)
             module_product_code = device['product_code']
-            # print(f'{slot}: {device['product_name']}')
-            # print(device)
 
-            # Check for ControlNet modules (product_code 22)
-            if module_product_code == 22:
-                ic(f'controlnet in slot {slot}')
-                # Add to future_scan if not already present
-                if module_serial_number in future_scan.keys():
-                    pass
-                else:
-                    future_scan[module_serial_number] = (
-                        f'{cip_path}/bp/{slot}/cnet',
-                        False  # Initially set to False (not scanned)
-                    )
-            if module_product_code in (93, 22, 166):  # processor, controlnet or ethernet
+            if module_product_code in intellectual_module or module_product_code == plc_module:  # controlnet or ethernet
                 ic(f'Intelligent in slot {slot}')
                 p = f'{cip_path}/bp/{slot}'
                 plc_driver = CIPDriver(p)
                 plc_driver.open()
+                # https://www.plctalk.net/threads/rockwell-plc-chassis-serial-number-rs-logix.86426/
                 backplane_serial_number_raw = plc_driver.generic_message(
                     service=Services.get_attributes_all,
                     class_code=0x66,
@@ -100,23 +82,31 @@ def scan_bp(cip_path):
                     route_path=True,
                 )
                 plc_driver.close()
-                discovered_bp_sn = ic(decode_serial_number(backplane_serial_number_raw.value))
+                bp_serial_current = ic(decode_serial_number(backplane_serial_number_raw.value))
+                if this_bp_sn == serial_unknown:
+                    this_bp_sn = bp_serial_current
+                elif this_bp_sn != bp_serial_current:
+                    raise BackplaneSerialNumberMissmatch(bp_serial_current, this_bp_sn)
 
-                if backplane_sn is None and discovered_bp_sn:
-                    backplane_sn = discovered_bp_sn
-                if backplane_sn is not None and discovered_bp_sn:
-                    assert backplane_sn == discovered_bp_sn
+            if module_product_code == controlnet_module:
+                ic(f'controlnet in slot {slot}')
+                cn_modules_paths[module_serial_number] = f'{cip_path}/bp/{slot}/cnet'
 
         except  ResponseError:
             ic(f'no module in slot {slot} or no slot')
             continue
-        devices.append(device)
+        # except AlreadyScanned:
+        #     ic(f'Backplane {discovered_bp_sn} has already been scanned. Skip it')
+        #     skip_this_bp = True
+        #     break
 
     driver.close()
-    return backplane_sn, future_scan, devices
+
+    return this_bp_sn, this_bp, cn_modules_paths
 
 
-def scan_cn(cip_path):
+def scan_cn(cip_path, format='', exclude_bp_sn=''):
+    cn_modules_paths = {}
     found_controlnet_nodes = []
     print(f'Scaning {cip_path}...')
     for cnet_node_num in range(100):
@@ -125,17 +115,17 @@ def scan_cn(cip_path):
         try:
             driver = CIPDriver(target)
             driver.open()
-            for slot in range(13):
+            for slot in range(14):
                 try:
                     device = driver.get_module_info(slot)
                     # print(f'Slot {slot}')
                     # print(device)
 
-                    if device['product_code'] == 22:
+                    if device['product_code'] == controlnet_module:
                         print(f'ControlNet module at slot {cnet_node_num}/{slot}')
                         if not cnet_node_num in found_controlnet_nodes:
                             found_controlnet_nodes.append(cnet_node_num)
-                        # future_scan.append(f'{entry_point}/bp/{slot}/cnet/')
+                        cn_modules_paths[device['serial']] = f'{cip_path}/{cnet_node_num}/bp/0'
                         pass
                 except ResponseError:
                     pass
@@ -144,7 +134,7 @@ def scan_cn(cip_path):
         except ResponseError:
             print('.', end='')
             driver.close()
-    return found_controlnet_nodes
+    return found_controlnet_nodes, cn_modules_paths
 
 
 def decode_serial_number(data):
@@ -169,30 +159,30 @@ def decode_serial_number(data):
     return hex_serial
 
 
-if __name__ == '__main__':
-    # this function here jast an example for coding purpose
-    # Example usage decode_serial_number
-    # data = b'\x00\x08\x00\x00\x00\x00\x04\x00\x00\x00\x01\x01\xef\xa5\x1a\x00\x00'
-    # decoded_serial = decode_serial_number(data)
-    # print(f"Decoded Serial Number: {decoded_serial}")
+def discover(entry_point):
+    bp_sn, bp, cn_path = scan_bp(test_entry)
+    assert type(cn_path) is dict
+    if len(cn_path):
+        # scan controlnet
+        for cn_serial, cip_path in cn_path.items():
+            cn_nodes, cn_nodes_paths = scan_cn(cip_path)
+            print(cn_nodes)
+            if len(cn_nodes) > 1:
+                for cn_serial, cip_path in cn_nodes_paths.items():
+                    bp_sn, bp, cn_path = scan_bp(cip_path)
+                    print(bp_sn, bp, cn_path)
 
+    else:
+        print(f'Single backplane system found')
+        pass
+
+
+
+if __name__ == '__main__':
     print('Scanner lib standalone running')
-    ic.disable()
+    # ic.disable()
 
     test_entry = '11.80.18.1'
-    # bp_sn, fs = scan_bp(test_entry)
-    # ic(bp_sn)
-    # ic(fs)
-    # for controlnet_sn in fs.keys():
-    #     cip_path, already_scanned = fs[controlnet_sn]
-    #     if not already_scanned:
-    #         nodes = scan_cn(cip_path)
-    #     print(nodes)
-    #
-    # pass
+    discover(test_entry)
 
-    d = Discover(test_entry)
-    d.run()
     pass
-    print(d.cn_nodes)
-    print(d.bps)
